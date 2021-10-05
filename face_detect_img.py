41af2853e288d9cad6eab4a8a42508be18eb64ca
Abhinav tembulkar <abhinavtembulkar@gmail.com>
2021-10-05 16:09:03 +0530

    Code improvements in face_detect_img.py

--- face_detect_img.py
+++ face_detect_img.py
@@ -5,13 +5,16 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read the input image
 img = cv2.imread('test.jpg')
+#convert 
+img = cv2.cvtColor(img,cv2.COLOR_RGB2BGR)
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # Detect faces
-faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+faces = face_cascade.detectMultiScale(gray, 1.6, 18)
 # Draw rectangle around the faces
 for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    print(img.shape)
+    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 255), 2)
 # Display the output
-cv2.imshow('img', img)
-cv2.waitKey()
+cv2.imshow('output', img)
+cv2.waitKey(0)
